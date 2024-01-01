1556df936f5ba0991f48188cfbdafe7d865b9317
tiago1820 <tiago.zdo@gmail.com>
2024-01-01 05:00:55 -0300

    let and const

---
+++ cap-02/index.js
@@ -0,0 +1,70 @@
+// Example 1
+// var framework = "Angular";
+// var framework = "React";
+// console.log(framework);
+
+// Example 2
+// let language = "JavaScript!";
+// let language = "Ruby!";
+// console.log(language);
+
+// Example 3
+// const PI = 3.141593;
+// PI = 3.0; //lança um erro
+// console.log(PI);
+
+// Example 4
+// const jsFramework = {
+//     name: "Angular"
+// };
+
+// jsFramework.name = "React";
+
+// jsFramework = {
+//     name: "Vue"
+// };
+
+// Example 5
+/*let movie = "Lord of the Rings"; // {1}
+// var movie = "Batman vs Superman"; // error
+function starWarsFan() {
+    const movie = "Star Wars"; // {2}
+    return movie;
+}
+
+function marvelFan() {
+    movie = "The Avengers"; //{3}
+    return movie;
+}
+
+function blizzardFan() {
+    const isFan = true;
+    let phrase = "Warcraft"; // {4}
+    console.log("Before if: " + phrase);
+    if(isFan) {
+        let phrase = "initial text"; //{5}
+        phrase = "For the Horde!"; //{6}
+        console.log("Inside if: " + phrase);
+    }
+    phrase = "For the Alliance!"; //{7}
+    console.log("After if: " + phrase);
+}
+console.log(movie);//{8}
+console.log(starWarsFan());//{9}
+console.log(marvelFan()); //{10}
+console.log(movie); //{11}
+blizzardFan(); // {12}
+*/
+
+// continue:page 54
+
+
+
+
+
+
+
+
+
+
+
